a71edc065a7895e9491d67a53b67faea713722bf
lee <lyhdra99@svcorps.dev>
2020-08-16 21:20:38 +0900

    init

---
+++ lib/flutter_auto_scale.dart
@@ -0,0 +1,22 @@
+library flutter_auto_scale;
+import 'dart:core';
+import 'package:flutter/material.dart';
+
+class FAScale {
+
+  static double _screenWidth = 1080.0;
+  static double _screenHeight = 1920.0;
+
+  void init(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    _screenWidth = mediaQuery.size.width;
+    _screenHeight = mediaQuery.size.height;
+  }
+
+  double getWidth(double percentage) => _screenWidth * (percentage / 100);
+
+  double getHeight(double percentage) => _screenHeight * (percentage / 100);
+
+  double sacle(double percentage) => percentage / 100 * (getHeight(percentage) + getWidth(percentage));
+
+}
